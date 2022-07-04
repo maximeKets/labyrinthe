473054183169a3aee16acabcd1df3eb953e0cc04
maximeKets <maximekets80@gmail.com>
2022-07-04 14:55:35 +0200

    Feature : - Create Labyrinthe

--- app.js
+++ app.js
@@ -3,11 +3,53 @@ const container = document.getElementById("container");
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+    for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
+        let idCell = document.createElement("p");
+        cell.innerText = (c+1);
         container.appendChild(cell).className = "grid-item";
-    };
-};
+        cell.appendChild(idCell).setAttribute("id",c+1 ) ;
+    }
+}
+ function makeWall(caseId){
 
-makeRows(16, 16);
+    for (let i = 0; i < caseId.length; i++) {
+        let wallId = document.getElementById(caseId[i])
+        let wall = wallId.parentElement;
+        wall.className="wall" }
+    }
+
+function makeGoal(caseId){
+
+        let wallId = document.getElementById(caseId)
+        let wall = wallId.parentElement;
+        wall.className="goal";
+}
+function makeStart(caseId){
+
+    let wallId = document.getElementById(caseId)
+    let wall = wallId.parentElement;
+    wall.className="start";
+}
+
+function goTop(caseId){
+
+}
+function goRight(){
+    return caseId+colls
+
+}
+function goDown(){
+
+}
+function goLeft(){
+
+}
+function startGame(rows, colls){
+    makeRows(rows, colls);
+    let chemin = [];
+    makeWall([2,9,16,30,37,11,18,32,12,26,27,41,14 ]);
+    makeGoal(19);
+    makeStart(1)
+}
+startGame(6, 7);
